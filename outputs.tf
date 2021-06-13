6e01c3d69cce7693e5f0ef325e5c3db55db03129
Cheeky.Bob <tom.stranger2@gmail.com>
2021-06-13 14:47:52 +0200

    change resource to data source

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "config" {
+  # required by official S3 backend
+  # https://www.terraform.io/docs/language/settings/backends/s3.html
   value = {
     bucket         = aws_s3_bucket.bucket.bucket
-    region         = aws_region.current.region
+    region         = data.aws_region.current.region
     role_arn       = aws_iam_role.role.arn
     dynamodb_table = aws_dynamodb_table.table.name
   }
