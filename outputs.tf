8196df0eb2f3df0088d733aa91f68fec2f3ebcc9
Cheeky.Bob <tom.stranger2@gmail.com>
2021-06-13 15:24:10 +0200

    Fix argument name

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "config" {
   # https://www.terraform.io/docs/language/settings/backends/s3.html
   value = {
     bucket         = aws_s3_bucket.bucket.bucket
-    region         = data.aws_region.current.region
+    region         = data.aws_region.current.name
     role_arn       = aws_iam_role.role.arn
     dynamodb_table = aws_dynamodb_table.table.name
   }
